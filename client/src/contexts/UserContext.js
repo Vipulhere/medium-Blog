2cc089f6e0d17b19bc90e32a8bc3f1ffccbf6817
Vipulhere <krvipul28@gmail.com>
2023-05-22 05:51:15 +0530

    completed medium Feature clone

---
+++ client/src/contexts/UserContext.js
@@ -0,0 +1,187 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../images/Spinner.gif";
+
+const UserContext = createContext();
+
+export default function UserContextProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+  const [myblogs, setMyBlogs] = useState([]);
+  const [blog, setBlog] = useState({});
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const FetchData = async () => {
+      try {
+        setLoading(true);
+        await FetchUser();
+        setLoading(false);
+      } catch (err) {
+        // console.log(err);
+        setLoading(false);
+      }
+    };
+    FetchData();
+  }, []);
+
+  const FetchAllBlogs = async () => {
+    const data = await axios.get(
+      "http://localhost:8800/api/blogs/fetchallblogs"
+    );
+    setBlogs(data.data.blogs);
+  };
+
+  // Fetch login user
+  // const FetchUser = async () => {
+  //   const data = await axios.get("http://localhost:8800/api/auth/getuser", {
+  //     headers: { "Authorization": localStorage.getItem("token") },
+  //   });
+  //   setUser(data.data);
+  // };
+
+
+  const FetchUser = async () => {
+    try {
+      const response = await axios.get("http://localhost:8800/api/auth/getuser", {
+        headers: { "Authorization": localStorage.getItem("token") },
+      });
+      const user = response.data;
+      setUser(user);
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately, e.g., display an error message
+    }
+  };
+  
+
+  // Fetch user blogs
+  const FetchBlogs = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8800/api/blogs/fetchuserblogs",
+        { headers: { "Authorization": localStorage.getItem("token") } }
+      );
+      const data = response.data;
+      setMyBlogs(data);
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately, e.g., display an error message
+    }
+  };
+  
+
+  // Edit user
+  const HandleEditUser = async (edit) => {
+    try {
+      await axios.put(
+        `http://localhost:8800/api/auth/update/${edit?._id}`,
+        edit,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem("token"),
+          },
+        }
+      );
+      FetchUser();
+    } catch (err) {
+      console.log(err.data);
+    }
+  };
+
+  // Delete user
+  const HandleDeleteUser = async (edit) => {
+    try {
+      await axios.delete(`http://localhost:8800/api/auth/delete/${edit._id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": localStorage.getItem("token"),
+        },
+      });
+      FetchUser();
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately, e.g., display an error message
+    }
+  };
+  
+
+  // Fetch Blog by Id
+  const FetchBlogByID = async (id) => {
+    try {
+      const response = await axios.get(`http://localhost:8800/api/blogs/fetchblog/${id}`);
+      const blog = response?.data[0];
+      setBlog(blog);
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately, e.g., display an error message
+    }
+  };
+  
+
+  // Edit Blog
+  const HandleEdit = async (edit) => {
+    try {
+      await axios.put(
+        `http://localhost:8800/api/blogs/updateblog/${edit._id}`,
+        edit,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem("token"),
+          },
+        }
+      );
+      FetchBlogs();
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately, e.g., display an error message
+    }
+  };
+  
+
+  // Delete Blog
+  const HandleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8800/api/blogs/deleteblog/${id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": localStorage.getItem("token"),
+        },
+      });
+      FetchBlogs();
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately, e.g., display an error message
+    }
+  };
+  
+
+  if (loading) return <img src={Spinner} className="Loader" />;
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        FetchAllBlogs,
+        FetchUser,
+        HandleEditUser,
+        HandleDeleteUser,
+        HandleEdit,
+        HandleDelete,
+        FetchBlogs,
+        FetchBlogByID,
+        myblogs,
+        blog,
+        blogs,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUserContext() {
+  return useContext(UserContext);
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.js";
+import blogsRouter from "./routes/blogs.js";
+import uploadRouter from "./middlewares/uploadImg.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+dotenv.config();
+
+const app = express();
+const port = 8800;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// app.use(cors({origin:"http://localhost:3000",
+//   methods:["get","post","delete","put","fetch"],
+//   credentials:true
+// }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+// Middlewares
+
+// Routes
+app.use("/api/auth", authRouter);
+app.use("/api/blogs", blogsRouter);
+app.use("/api/upload", uploadRouter);
+
+// Static/s
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"), {
+//     function(err) {
+//       res.status(500).send(err);
+//     },
+//   });
+// });
+
+const Connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    throw err;
+  }
+};
+
+Connect().then(() => {
+  app.listen(port, () => {
+    console.log("Example app running at", port);
+  });
+});
